e4edcc3b5f4878923ed7f74018698e9b076d480f
nj <nj@billetto.dk>
2010-08-18 10:53:53 +0200

    Initial commit

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '51f3f36fd096e8d223661e8b8389cc3fca5de0e59ba9986dae6ee21db03fd581b44f007ed67ea78e041ec459270d1f698e4460b1b5dbe3d5a61005ccef019cac'
---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5ee5248f022a2f78a4e92d24e1956580d237b9c1ef93690faee19569e340f911a8b7a5528b67f9d44bbe926d3a8a50626bb573a14dc2afcea5b8b4eef0537b30'
